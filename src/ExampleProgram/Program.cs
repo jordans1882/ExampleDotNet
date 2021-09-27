d6df4d4685569aba7f8af9fb9c438cf2d2da1e86
jordans1882 <jordans1882@gmail.com>
2021-09-27 13:44:14 -0600

    Add simple program

---
+++ src/ExampleProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExampleProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
